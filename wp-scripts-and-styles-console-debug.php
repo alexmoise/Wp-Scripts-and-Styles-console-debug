312819d53451ddf61dc8f7656b99a4251da0bb0c
Alex Moise <alex@moise.pro>
2022-09-19 12:42:16 +0300

    Eliminate jQuery dependence
    (why was it relying on jQuery just for getting the document.ready anyway?)

--- wp-scripts-and-styles-console-debug.php
+++ wp-scripts-and-styles-console-debug.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/alexmoise/Wp-Scripts-and-Styles-console-debug
  * GitHub Plugin URI: https://github.com/alexmoise/Wp-Scripts-and-Styles-console-debug
  * Description: A simple plugin to quickly output all WordPress styles and scripts in browser console, for debugging purposes. It shows handle and filename, and for JSs the location (header or footer). 
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
@@ -15,7 +15,7 @@ add_action( 'wp_footer', 'motils_console_log_scripts' );
 function motils_console_log_scripts() {
 	global $wp_scripts; $obno = 0;
 	echo "\r\n <script type='text/javascript'> \r\n /* <![CDATA[ */ \r\n ";
-		echo "jQuery(document).ready(function() {\r\n"; 
+		echo "document.addEventListener(\"DOMContentLoaded\", function() {\r\n"; 
 		echo "console.log('SCRIPTS:');\r\n";
 		foreach( $wp_scripts->queue as $handle ) {
 			$obno ++;
@@ -33,7 +33,7 @@ add_action( 'wp_footer', 'motils_console_log_styles' );
 function motils_console_log_styles() {
 	global $wp_styles; $obno = 0;
 	echo "\r\n <script type='text/javascript'> \r\n /* <![CDATA[ */ \r\n ";
-		echo "jQuery(document).ready(function() {\r\n"; 
+		echo "document.addEventListener(\"DOMContentLoaded\", function() {\r\n"; 
 		echo "console.log('STYLES:');\r\n";
 		foreach( $wp_styles->queue as $handle ) {
 			$obno ++;
